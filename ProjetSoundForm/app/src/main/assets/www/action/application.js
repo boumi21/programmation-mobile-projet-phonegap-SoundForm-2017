53709caf38e93519075367a47385af05bc3a1f06
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2017-11-21 10:52:20 -0500

    Application
    Ajouter sample ancre

--- ProjetSoundForm/app/src/main/assets/www/action/application.js
+++ ProjetSoundForm/app/src/main/assets/www/action/application.js
@@ -2,8 +2,18 @@ var applicationSoundForm =
 {
     sampleDAO : new SampleDAO(),
     lancer:function()
-        {
-            $(window).on('hashchange',$.proxy(this.naviguer, this));
-            this.naviguer();
-        },
+    {
+        $(window).on('hashchange',$.proxy(this.naviguer, this));
+        this.naviguer();
+    },
+    naviguer:function()
+    {
+        var ancre = window.location.hash;
+
+         else if(ancre.match(/^#ajouter-sample/))
+                {
+                    this.ajouterSampleVue.afficher();
+                }
+    }
+}
 applicationListeNoel.lancer();
