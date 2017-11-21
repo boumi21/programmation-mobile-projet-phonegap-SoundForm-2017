ba4092c69ba71b61b82ceda8aada03dffb23f72e
lucas <lucas.paulin@hotmail.fr>
2017-11-20 19:13:52 -0500

    Création du modele Sample

---
+++ ProjetSoundForm/app/src/main/assets/www/action/modeles/Sample.js
@@ -0,0 +1,8 @@
+var Sample = function(id = undefined, nom, forme, fichier, couleur, dimension){
+	this.id = id;
+	this.nom = nom;
+	this.forme = forme;
+	this.fichier = fichier;
+	this.couleur = couleur;
+	this.dimension = dimension;
+}
