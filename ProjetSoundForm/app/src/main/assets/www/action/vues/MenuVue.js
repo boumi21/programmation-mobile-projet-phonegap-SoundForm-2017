6bdc5b17b8f66258cd46f79b62fde1734f243997
boumi21 <glorkpatate@yahoo.be>
2017-11-25 23:05:31 -0500

    HammerJS + HammerTimeJS
    Ajout des 2 framework Hammer.
    Le premier permet de gérer les gestures et le second permet d'enlever la latence de 300ms lorsque l'on appuie sur un bouton.

--- ProjetSoundForm/app/src/main/assets/www/action/vues/MenuVue.js
+++ ProjetSoundForm/app/src/main/assets/www/action/vues/MenuVue.js
@@ -1,9 +1,15 @@
 var MenuVue = function()
 {
+
     this.afficher = function()
     {
         $("body").html(MenuVue.pageMenu);
         var htmlMenu = $("#page-menu");
+        var myElement = document.getElementById('testHammer');
+        var mc = new Hammer(myElement);
+        mc.on("panleft panright tap press swiperigh swipeleft rotate pinch", function(ev) {
+    myElement.textContent = ev.type +" gesture detected.";
+});
     }
 }
 MenuVue.pageMenu = $("#page-menu").html();
