86471c858f0e92cfcb10f5736ebc727b9b72877a
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2017-11-30 17:10:11 -0500

    fabric.js carre
    Mise en place d'un carrer
    utilisation de la librairie fabric.js
    Dans menuVue.js avec la balise script
    dans l'index avec le canvas

--- ProjetSoundForm/app/src/main/assets/www/action/vues/MenuVue.js
+++ ProjetSoundForm/app/src/main/assets/www/action/vues/MenuVue.js
@@ -6,6 +6,21 @@ var MenuVue = function()
         $("body").html(MenuVue.pageMenu);
         var htmlMenu = $("#page-menu");
 
+
+
+                        var canvas = new fabric.Canvas('canvas-liste-sample');
+                        var rect = new fabric.Rect({
+                            left: 100,
+                            top: 100,
+                            fill: 'red',
+                            width: 100,
+                            height: 100
+                        });
+
+                        canvas.add(rect);
+
+
+
 		document.getElementById('playButton').onclick = function(e){
 		            move('.box')
 		          .set('background-color', 'red')
@@ -16,12 +31,12 @@ var MenuVue = function()
 		          .end();
 
 		        };
-        
+
         var myElement = document.getElementById('testHammer');
         var mc = new Hammer(myElement);
         mc.on("panleft panright tap press swiperigh swipeleft rotate pinch", function(ev) {
-    myElement.textContent = ev.type +" gesture detected.";
-});
+            myElement.textContent = ev.type +" gesture detected.";
+        });
     }
 }
 MenuVue.pageMenu = $("#page-menu").html();
