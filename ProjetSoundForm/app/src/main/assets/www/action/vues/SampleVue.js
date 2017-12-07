a1ef23c6ae5d3acd543b997940b7884e315e7271
lucas <lucas.paulin@hotmail.fr>
2017-12-07 17:26:03 -0500

    Ajout des rectangle dans la vue du sample

--- ProjetSoundForm/app/src/main/assets/www/action/vues/SampleVue.js
+++ ProjetSoundForm/app/src/main/assets/www/action/vues/SampleVue.js
@@ -4,7 +4,18 @@ var SampleVue = function(){
         var canvas = new fabric.Canvas('sampleur');
 
         for (sample of listeSample){
-            
+            if (sample.forme == "Rectangle"){
+                var rect = new fabric.Rect({
+                    left: sample.positionX,
+                    top: sample.positionY,
+                    fill: sample.couleur,
+                    width: sample.dimension,
+                    height: sample.dimension
+                });
+
+                canvas.add(rect);
+
+            }
         }
     }
 }
