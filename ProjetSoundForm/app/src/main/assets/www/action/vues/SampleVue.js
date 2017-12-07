404b891d373536e8f809fcfadb830d3b5b483bf2
lucas <lucas.paulin@hotmail.fr>
2017-12-07 17:23:40 -0500

    Création de la vue du sample

---
+++ ProjetSoundForm/app/src/main/assets/www/action/vues/SampleVue.js
@@ -0,0 +1,10 @@
+var SampleVue = function(){
+
+    this.afficher = function(listeSample){
+        var canvas = new fabric.Canvas('sampleur');
+
+        for (sample of listeSample){
+            
+        }
+    }
+}
