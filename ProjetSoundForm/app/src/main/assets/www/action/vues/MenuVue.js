933deb6b1d85d82cdcbf4840838c3c12367c407b
lucas <lucas.paulin@hotmail.fr>
2017-12-19 16:30:37 -0500

    supression du test de fabricjs

--- ProjetSoundForm/app/src/main/assets/www/action/vues/MenuVue.js
+++ ProjetSoundForm/app/src/main/assets/www/action/vues/MenuVue.js
@@ -5,24 +5,6 @@ var MenuVue = function()
     {
         $("body").html(MenuVue.pageMenu);
         var htmlMenu = $("#page-menu");
-
-
-
-                        var canvas = new fabric.Canvas('canvas-liste-sample');
-                        var rect = new fabric.Rect({
-                            left: 100,
-                            top: 100,
-                            fill: '#808080',
-                            width: 100,
-                            height: 100
-                        });
-
-                        canvas.add(rect);
-
-
-
-
-
         var myElement = document.getElementById('testHammer');
         var mc = new Hammer(myElement);
         mc.on("panleft panright tap press swiperigh swipeleft rotate pinch", function(ev) {
