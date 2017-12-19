67eb5483ec77731985ad31536e580a7e3a5a628f
lucas <lucas.paulin@hotmail.fr>
2017-12-19 16:32:58 -0500

    Edition des formes rendu impossible dans la page du sampleur
    L'édition dera possiblle uniquement dans la page de l'éditeur

--- ProjetSoundForm/app/src/main/assets/www/action/vues/SampleVue.js
+++ ProjetSoundForm/app/src/main/assets/www/action/vues/SampleVue.js
@@ -1,10 +1,12 @@
 var SampleVue = function(){
 
     this.afficher = function(listeSample){
-        var canvas = new fabric.Canvas('sampleur');
+        $("body").html(SampleVue.pageSampleVue);
+
+        var canvas = new fabric.Canvas('canvasSampleur',{width: 1000, height:1100});
 
         for (sample of listeSample){
-            if (sample.forme == "Rectangle"){
+            if (sample.forme == "rectangle"){
                 var rect = new fabric.Rect({
                     left: sample.positionX,
                     top: sample.positionY,
@@ -13,9 +15,18 @@ var SampleVue = function(){
                     height: sample.dimension
                 });
 
+                rect.lockMovementY = true;
+                rect.lockMovementX = true;
+                rect.lockScalingX = true;
+                rect.lockScalingY = true;
+                rect.lockRotation = true;
+                rect.hasBorders = false;
+                rect.hasControls = false;
+                rect.hasRotatingPoint = false;
+
                 canvas.add(rect);
 
-            } else if(sample.forme == "Triangle"){
+            } else if(sample.forme == "triangle"){
                 var triangle = new fabric.Circle({
                     left: sample.positionX,
                     top: sample.positionY,
@@ -24,9 +35,18 @@ var SampleVue = function(){
                     height: sample.dimension
                 });
 
+                triangle.lockMovementX = true;
+                triangle.lockMovementY = true;
+                triangle.lockScalingX = true;
+                triangle.lockScalingY = true;
+                triangle.lockRotation = true;
+                triangle.hasBorders = false;
+                triangle.hasControls = false;
+                triangle.hasRotatingPoint = false;
+
                 canvas.add(triangle);
 
-            } else if(sample.forme == "Cercle") {
+            } else if(sample.forme == "cercle") {
                 var cercle = new fabric.Circle({
                     radius: sample.dimension,
                     left: sample.positionX,
@@ -34,8 +54,18 @@ var SampleVue = function(){
                     fill: sample.couleur
                 });
 
+                cercle.lockMovementX = true;
+                cercle.lockMovementY = true;
+                cercle.lockScalingX = true;
+                cercle.lockScalingY = true;
+                cercle.lockRotation = true;
+                cercle.hasBorders = false;
+                cercle.hasControls = false;
+                cercle.hasRotatingPoint = false;
+
                 canvas.add(cercle);
             }
         }
     }
 }
+SampleVue.pageSampleVue = $("#page-sample").html();
